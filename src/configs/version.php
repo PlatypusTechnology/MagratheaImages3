871f238ec2ba35770ce4d6dc46e44e6ad371bb33
Paulo Martins <paulovelho@paulovelho.com>
2026-08-07 00:05:26 -0300

    swagger updated

--- src/configs/version.php
+++ src/configs/version.php
@@ -1,9 +1,9 @@
 <?php
 // auto generated file
 $version = [
-  'timestamp' => '2026-08-06T22:23:25',
-  'commit' => 'b7110a8b8d4cdd600e7df9fc8db481d8cb9ff187',
-  'commit_author' => 'Paulo Henrique Martins <paulovelho@paulovelho.com>',
-  'commit_date' => '2026-08-06T22:18:52',
-  'commit_line' => 'b7110a8 Merge pull request #8 from PlatypusTechnology/feature/uuid-implementation'
+  'timestamp' => '2026-08-07T00:05:19',
+  'commit' => '0aabab5cda919068e3515bf605a784dfd7217ce8',
+  'commit_author' => 'Paulo Martins <paulovelho@paulovelho.com>',
+  'commit_date' => '2026-08-06T22:36:28',
+  'commit_line' => '0aabab5 swagger updated'
 ];
